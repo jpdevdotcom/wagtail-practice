aa182da9209a9645d00e29649192cdcfa119887a
Jan Phillip Dacallos <jphillipdacallos@gmail.com>
2025-06-09 21:04:32 +0800

    chore: add vue_quasar_wagtail prac

---
+++ vue_quasar_wagtail/myproject/myproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-cjczskcm#o+pz0ol3r23)4x9z$8m#k(o6avwj&jugq-y^j=xvc"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
